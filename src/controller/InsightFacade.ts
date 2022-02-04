8880dfee67808a965af5422b02790c9d3470c251
Diwakar Gupta <goodboydiwaker@gmail.com>
2022-02-04 10:27:11 -0800

    Updated Section class, addDataset parses and creates a new section

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,72 +1,65 @@
 import JSZip, {loadAsync} from "jszip";
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightResult} from "./IInsightFacade";
-import Section from "./Section";
+import Section from "../../../project_team659/src/controller/Section";
 
 
 export default class InsightFacade implements IInsightFacade {
+	private datasetList = [];
+	public courseList = [];
 
-	// eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-	addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
-		let sub;
-		let avg;
-		let prof;
-		let title;
-		let pass;
-		let fail;
-		let audit;
-		let cid;
-		let uuid;
-		let year;
+	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
+		let counter = 0;
+		let sub: string;
+		let avg: number;
+		let prof: string;
+		let title: string;
+		let pass: number;
+		let fail: number;
+		let audit: number;
+		let cid: string;
+		let uuid: string;
+		let year: number;
+		let section: Section;
 		let zip = new JSZip();
 		zip.loadAsync(content, {base64: true}); // Store data as JSzip
 		zip.forEach(function(relativePath, file) { // For each Zip object
-			let cont = file.async("string").then // Read it, store it as a string
-				// eslint-disable-next-line no-unexpected-multiline
-			((filecontent) => JSON.parse(filecontent, (key, value) => {
-				switch (key) {
-					case "Subject:":
-						sub = value;
-					case "Course":
-						cid = value;
-					case "Avg":
-						avg = value;
-					case "Professor":
-						prof = value;
-					case "Title":
-						title =  value;
-					case "Pass":
-						pass = value;
-					case "Fail":
-						fail = value;
-					case "Audit":
-						audit = value;
-					case "id":
-						uuid = value;
-					case  "Section":
-						if (value === "overall") {
+			let cont: Promise<"string">;
+			cont = file.async("string").then((filecontent) => JSON.parse(filecontent,
+				(key, value) => {
+					switch (key) {
+						case "Subject:": sub = value;
+						case "Course": cid = value;
+						case "Avg": avg = value;
+						case "Professor": prof = value;
+						case "Title": title = value;
+						case "Pass": pass = value;
+						case "Fail": fail = value;
+						case "Audit": audit = value;
+						case "id": uuid = value;
+						case  "Section": if (value === "overall") {
 							year = 1990;
 						}
-					case "Year":
-						year = value;
-				}
-			}));// store it as our data type
-
+						case "Year": year = value;
+					}
+				}));
+			if (sub !== null && cid !== null && avg !== null && prof !== null && title !== null && pass !== null &&
+				fail !== null && audit !== null && uuid !== null && year !== null) {
+				section = new Section(sub, cid, avg, prof, title, pass, fail, audit, uuid, year);
+				counter++;
+			}
 		});
 		return Promise.resolve([]);
 	}
 
-	// eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-	listDatasets(): Promise<InsightDataset[]> {
+	public listDatasets(): Promise<InsightDataset[]> {
 		return Promise.resolve([]);
 	}
 
-	// eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-	performQuery(query: unknown): Promise<InsightResult[]> {
+	public performQuery(query: unknown): Promise<InsightResult[]> {
 		return Promise.resolve([]);
 	}
 
-	// eslint-disable-next-line @typescript-eslint/explicit-member-accessibility
-	removeDataset(id: string): Promise<string> {
+	public removeDataset(id: string): Promise<string> {
 		return Promise.resolve("");
 	}
 }
